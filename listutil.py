150a53d30b663f9394b50cf8f5aede468cb2df15
Katapon <q.katapol@gmail.com>
2018-09-07 22:13:08 +0700

    Complete

--- listutil.py
+++ listutil.py
@@ -16,7 +16,7 @@ def count_unique(list):
    >>> count_unique([ ])
    0
    """
-    if len(list) is None:
+    if list == None:
         return None
     if len(list) == 0:
         return 0
@@ -29,3 +29,42 @@ def count_unique(list):
             compare = i
 
     return count
+
+def binary_search(list, element):
+    """Search the element in list that already sorted by using binary search.
+
+       :param list:  list of elements to find elements of
+              element: element in list that want to find
+       :return: the index  of elements that want to find from list,
+                if not, return 'Not found'
+
+       Examples:
+       >>> binary_search([1, 2, 3, 4, 5, 6, 7], 1)
+       0
+       >>> binary_search([1, 2, 3, 4, 5, 6], 6)
+       5
+       >>> binary_search([1, 2, 3, 4, 5, 6], 7)
+       'Not found'
+       >>> binary_search([], 4)
+       'List is empty'
+       """
+
+    if element == None:
+        raise TypeError('Search element must not be none')
+    if len(list) == 0:
+        return 'List is empty'
+
+    list.sort()
+    leftside = 0
+    rightside = len(list)-1
+    while leftside <= rightside:
+        middle = int(((leftside+rightside)/2))
+        if list[middle] == element:
+            return middle
+        elif element < list[middle]:
+            rightside = middle-1
+        else:
+            leftside = middle+1
+
+    return 'Not found'
+
--- tests/listutil_test.py
+++ tests/listutil_test.py
@@ -1,5 +1,5 @@
 import unittest
-from listutil import count_unique
+from listutil import count_unique, binary_search
 
 class TestCountElement(unittest.TestCase):
     """
@@ -21,13 +21,10 @@ class TestCountElement(unittest.TestCase):
         self.assertEqual(10, count_unique(fontlist))
         self.assertEqual(10, count_unique(mathlist))
 
-    ## Failcase because is None
-    # def test_impossible_case(self):
-    #     list == None
-    #     except:
-    #         print("Error list is null")
-    #
-    #     self.assertEqual(None, count_unique(fontlist))
+
+    def test_impossible_case(self):
+        list = None
+        self.assertIsNone(count_unique(list))
 
 
     def test_huge_list_case(self):
@@ -36,3 +33,30 @@ class TestCountElement(unittest.TestCase):
             list.append(i)
 
         self.assertEqual(999998, count_unique(list))
+
+
+class TestBinarySearch(unittest.TestCase):
+    """
+    Test binary_search method with borderline cases, typical cases, impossible cases and extreme cases
+    """
+    def test_empty_case(self):
+        list = []
+        self.assertEqual('List is empty', binary_search(list, 4))
+
+    def test_find_not_found(self):
+        list = [1, 2, 3, 4, 5, 6, 7]
+        self.assertEqual('Not found', binary_search(list, 8))
+
+    def test_element_is_none(self):
+        list = [1, 2, 3, 4, 5, 6, 7]
+        binary_search(list, None)
+        self.assertRaises(TypeError)
+
+    def test_huge_list_case(self):
+        list = []
+        for i in range(1,999999):
+            list.append(i)
+        self.assertEqual(49999,binary_search(list, 50000))
+
+if __name__ == "__main__":
+    unittest.main()
